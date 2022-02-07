bef1784ac0972df8dcf3ed7637072a75ecfe73df
FranklysG <tunele095@gmail.com>
2022-02-07 00:04:12 -0300

    refatorando o codigo usando conseitos de clean code

--- index.js
+++ index.js
@@ -1,71 +1,79 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
+
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
-  await page.goto('https://ge.globo.com/agenda/#/todos/');
-  // await page.click('[data-testid="show-more-text"]');
-  await page.evaluate(() => {
-    // executar toda funçao no navegador
-    // pegar toda a seção com os jogos de hoje
-    const gamesToday = [];
-    const feedEsports = document.querySelector('[data-testid="sport-feed-container"]');
-    const champions = feedEsports.querySelectorAll('[data-testid="group-by-championship"]');
-    [...champions].map(element => {
 
-      // pega o nome do campeonato
-      const champion = element.querySelector('div > span').innerHTML; 
-      
-      // pega todos os jogos de hoje
-      const matchToday = [];
-      const nodeList = element.querySelectorAll('div > a');
-      [...nodeList].map(element => {
-        // horario do jogo
-        matchHour = element.querySelectorAll('[data-testid="event-card-header-wrapper"] > div > span');
-        if([...matchHour].length === 0){
-          //  ao vivo
-          matchHour = element.querySelectorAll('[data-testid="event-card-header-wrapper"] > div > div > span');
-          if([...matchHour].length === 0){
-            // encerrado
-            matchHour = element.querySelectorAll('[data-testid="event-card-header-wrapper"] > div ');
-            if([...matchHour].length === 0){
-              // saiba como foi
-              matchHour = element.querySelectorAll('[data-testid="event-card-header-wrapper"] > div > a > span');
+  await page.setViewport({ width: 1366, height: 768});
+  await page.goto('https://ge.globo.com/agenda/#/todos/');
+  await page.evaluate(() => {
+
+    const events_today = [];
+    const sport_feed = document.querySelector('[data-testid="sport-feed-container"]');
+    const championships = sport_feed.querySelectorAll('[data-testid="group-by-championship"]');
+
+    championships.forEach((element) => {
+      // open all container's 
+      document.querySelectorAll('[data-testid="default-show-more-button"]').forEach(element => {
+        element.click();
+      });
+
+      // get all championship name
+      const championship_name = element.querySelector('div > span').innerHTML; 
+      const games_today = [];
+
+      const card_game = element.querySelectorAll('div > a');
+      card_game.forEach((element) => {
+
+        time = element.querySelectorAll('[data-testid="event-card-header-wrapper"] > div > span');
+        if([...time].length === 0){
+          //  in live
+          time = element.querySelectorAll('[data-testid="event-card-header-wrapper"] > div > div > span');
+          if([...time].length === 0){
+            // closed
+            time = element.querySelectorAll('[data-testid="event-card-header-wrapper"] > div ');
+            if([...time].length === 0){
+              // know how it was
+              time = element.querySelectorAll('[data-testid="event-card-header-wrapper"] > div > a > span');
             }
           }
         }
-        const hour = [...matchHour][0].innerText;
-        
-        // pega os jogos existentes por campeonato
-        const matchPerChampion = [];
-        const match = element.querySelectorAll('div > div');
-        [...match].map((element) => {
-            const nameScore = [];
-            const name = element.querySelectorAll('span > span');
-            [...name].map((name) => {
-              nameScore.push(name.innerHTML);
-            });
-            const score = element.querySelectorAll('div > span[data-testid="score-line-score"]');
-            [...score].map((score) => {
-              nameScore.push(score.innerHTML);
-            });
-            if(nameScore.length > 1){
-              matchPerChampion.push(nameScore);
-            }
-          }
-        );
 
-        matchToday.push({matchPerChampion, hour});
+        const game_time = [...time].shift().innerText;
+
+        // get all exists games
+        const match = element.querySelectorAll('div > div');
+        const championship_games = [];
+        match.forEach((element) => {
+          const team_name = element.querySelectorAll('span > span');
+          const how_was_game = [];
+          
+          team_name.forEach((name) => {
+            
+            const team_score = element.querySelectorAll('div > span[data-testid="score-line-score"]');
+            team_score.forEach((score) => {
+              how_was_game.push({'name': name.innerHTML, 'score': score.innerHTML});
+            });
+
+          });
+
+          if(how_was_game.length !== 0){
+            championship_games.push(how_was_game);
+          }
+        }
+        );
+        
+        if(championship_games.length !== 0){
+          games_today.push({championship_games, game_time});
+        }
       });
      
-      gamesToday.push({
-        'champion': champion,
-        'matchs': matchToday
-      });
+      events_today.push({championship_name,games_today});
     });
     
    
-    console.log(JSON.stringify(gamesToday));
+    console.log(JSON.stringify(events_today));
 
   });
 
